2212ae12e991f1bba4bec6e59cc6d7621e5918c0
TucksonDev <tucksondev@gmail.com>
2024-07-09 16:28:01 +0100

    Smol changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 // Only run this as a WASM if the export-abi feature is not set.
-#![cfg_attr(not(feature = "export-abi"), no_main)]
+#![cfg_attr(not(any(feature = "export-abi", test)), no_main)]
 extern crate alloc;
 
 #[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 
 use stylus_sdk::{alloy_primitives::U256, prelude::*, storage::StorageU256};
 
