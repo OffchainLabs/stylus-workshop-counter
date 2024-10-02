1eba83b04690e8444b8ed5e134df2e0973166d4e
TucksonDev <tucksondev@gmail.com>
2024-10-02 12:54:15 +0100

    Update testnode pin

--- scripts/tests/nitro-testnode.sh
+++ scripts/tests/nitro-testnode.sh
@@ -9,7 +9,7 @@ cd ../..
 git clone --recurse-submodules https://github.com/OffchainLabs/nitro-testnode.git
 cd ./nitro-testnode || exit
 # `release` branch.
-git checkout 8cb6b84e31909157d431e7e4af9fb83799443e00 || exit
+git checkout 2c2a97c2608ab0c83f2322af2a0dc2bf0b15ea31 || exit
 
 # Initialize test node
 ./test-node.bash --no-run --init || exit
